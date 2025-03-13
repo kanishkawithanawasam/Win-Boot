7fdd5b7e54bee63b9e6f318b584fb188a3971d86
kanishkawithanawasam <kanishkawithanawasam@gmail.com>
2025-03-13 13:47:01 +0000

    feat (ISO file selection): Implementation complete

--- src/main/java/com/kgsolutions/winboot/viewcontrollers/HomeViewController.java
+++ src/main/java/com/kgsolutions/winboot/viewcontrollers/HomeViewController.java
@@ -1,13 +1,22 @@
 package com.kgsolutions.winboot.viewcontrollers;
 
 
+import com.kgsolutions.winboot.controllers.MainController;
+import com.kgsolutions.winboot.models.Model;
 import com.kgsolutions.winboot.utils.DevicesReader;
 import com.kgsolutions.winboot.utils.USBListeningService;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -18,10 +27,20 @@ import java.util.List;
  */
 public class HomeViewController {
 
+    private MainController mainController;
+    private Model model;
+    private Stage stage;
+
     @FXML
     private ComboBox<String> comboBox;
+    @FXML
+    private Label isoFileLocationLabel;
 
-    public void initializeUsbListeningService() throws IOException {
+
+    public void init(MainController controller, Model model, Stage stage) throws IOException {
+        this.mainController = controller;
+        this.model = model;
+        this.stage = stage;
         DevicesReader devicesReader = new DevicesReader();
         this.updateUSBList(devicesReader.getDevices());
         this.startUsbListeningService();
@@ -39,5 +58,35 @@ public class HomeViewController {
     public void updateUSBList(List<String> usbList) {
         ObservableList<String> devices = FXCollections.observableArrayList(usbList);
         this.comboBox.setItems(devices);
+    }
+
+    @FXML
+    public void  setSelectedDevice(ActionEvent event) {
+        model.setSelectedDevice(comboBox.getSelectionModel().getSelectedItem());
+    }
+
+    @FXML
+    public void openFileSelectAction() {
+        FileChooser fileChooser = new FileChooser();
+
+        // Set extension filter for ISO files
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("ISO Files (*.iso)", "*.iso");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        // Open the dialog
+        File file = fileChooser.showOpenDialog(stage);
+
+        if (file != null) {
+            String[] pathComponents = file.getAbsolutePath().split("/");
+            isoFileLocationLabel.setText(pathComponents[pathComponents.length - 1]);
+            model.setSelectedIsoFile(file);
+        } else {
+            isoFileLocationLabel.setText("No file selected");
+        }
+    }
+
+    @FXML
+    public void startBootableDeviceCreationAction() {
+
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.kgsolutions.winboot {
-    requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires com.gluonhq.charm.glisten;
 
     // Open the package that contains the FXML controller to javafx.fxml
     opens com.kgsolutions.winboot.viewcontrollers to javafx.fxml;
@@ -11,4 +12,6 @@ module com.kgsolutions.winboot {
     // Export the main package
     exports com.kgsolutions.winboot;
     exports com.kgsolutions.winboot.viewcontrollers;
+    exports com.kgsolutions.winboot.controllers;
+    opens com.kgsolutions.winboot.controllers to javafx.fxml;
 }
